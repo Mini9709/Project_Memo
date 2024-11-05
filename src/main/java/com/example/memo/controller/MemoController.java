253780e192912d9fe97002e98d323cef4206cbfe
Mini9709 <tjdals9709@naver.com>
2024-11-05 16:42:32 +0900

    fix : 응답코드 세분화 및 예외 발생

--- src/main/java/com/example/memo/controller/MemoController.java
+++ src/main/java/com/example/memo/controller/MemoController.java
@@ -3,11 +3,11 @@ package com.example.memo.controller;
 import com.example.memo.dto.MemoRequestDto;
 import com.example.memo.dto.MemoResponseDto;
 import com.example.memo.entity.Memo;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("/memos")
@@ -15,8 +15,9 @@ public class MemoController {
 
     private final Map<Long, Memo> memoList = new HashMap<>();
 
+    //메모 작성
     @PostMapping
-    public MemoResponseDto createMemo(@RequestBody MemoRequestDto dto) {
+    public ResponseEntity<MemoResponseDto> createMemo(@RequestBody MemoRequestDto dto) {
         // 식별자가 1씩 증가하도록 구현
         Long memoId = memoList.isEmpty() ? 1 : Collections.max(memoList.keySet()) + 1;
 
@@ -26,35 +27,100 @@ public class MemoController {
         // Inmemory DB에 Memo 메모
         memoList.put(memoId, memo);
 
-        return new MemoResponseDto(memo);
+        return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.CREATED);
+
     }
 
+    // 메모 선택 목록 조회
     @GetMapping("/{id}")
-    public MemoResponseDto findMemoById(@PathVariable Long id) {
+    public ResponseEntity<MemoResponseDto> findMemoById(@PathVariable Long id) {
 
         Memo memo = memoList.get(id);
 
-        return new MemoResponseDto(memo);
+        if (memo == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.OK);
 
     }
 
+    //메모 전체 목록 조회
+    @GetMapping
+    public ResponseEntity<List<MemoResponseDto>> findAllMemos() {
+
+        // init List
+        List<MemoResponseDto> responseList = new ArrayList<>();
+
+        // HashMap<Memo> -> List<MemoResponseDto>
+        for (Memo memo : memoList.values()) {
+            MemoResponseDto responseDto = new MemoResponseDto(memo);
+            responseList.add(responseDto);
+        }
+
+        // Map to List (use stream)
+        // responseList = memoList.values().stream().map(MemoResponseDto::new).toList();
+
+        return new ResponseEntity<>(responseList, HttpStatus.OK);
+
+    }
+
+    // 메모 수정
     @PutMapping("/{id}")
-    public MemoResponseDto updateMemoById(
+    public ResponseEntity<MemoResponseDto> updateMemoById(
             @PathVariable Long id,
             @RequestBody MemoRequestDto dto
     ) {
         Memo memo = memoList.get(id);
 
+        if (memo == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        if (dto.getTitle() == null || dto.getContents() == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
         memo.update(dto);
 
-        return new MemoResponseDto(memo);
+        return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.OK);
 
     }
 
-    @DeleteMapping("/{id}")
-    public void deleteMemoById(@PathVariable Long id) {
+    @PatchMapping("/{id}")
+    public ResponseEntity<MemoResponseDto> updateTitle(
+            @PathVariable Long id,
+            @RequestBody MemoRequestDto dto
+    ) {
 
-        memoList.remove(id);
+        Memo memo = memoList.get(id);
+
+        // NPE 방지
+        if (memo == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        if (dto.getTitle() == null || dto.getContents() != null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        memo.updateTitle(dto);
+
+        return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.OK);
+
+    }
+    // 메모 삭제
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMemoById(@PathVariable Long id) {
+
+        // memoList에 Key값 여부 확인
+        if (memoList.containsKey(id)) {
+            memoList.remove(id);
+
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 
     }
 }
